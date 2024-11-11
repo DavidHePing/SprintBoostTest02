993303403b518b0e8b994cd6c23720816d145e22
Dav He <s991179@gmail.com>
2024-11-11 11:00:48 +0800

    refactor: rename application

--- src/main/java/com/example/SprintBoostTest02/Application.java
+++ src/main/java/com/example/SprintBoostTest02/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprintBoostTest02Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintBoostTest02Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
