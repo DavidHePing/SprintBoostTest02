26cee025f5ad5e716070822bb0e94a2537fca495
Dav He <s991179@gmail.com>
2024-11-11 22:48:38 +0800

    feat: set port 8081

---
+++ src/main/java/com/example/SprintBoostTest02/TestController.java
@@ -0,0 +1,17 @@
+package com.example.SprintBoostTest02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @ResponseBody
+    @GetMapping(value = "/Test")
+    public String testGet(){
+        return "Hello World";
+    }
+}
