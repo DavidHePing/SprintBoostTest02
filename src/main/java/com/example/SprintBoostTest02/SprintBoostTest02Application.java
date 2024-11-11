9deff6810733ef6ce7f3e60903fb142b468a9d94
Dav He <s991179@gmail.com>
2024-11-11 10:59:36 +0800

    feat: init

---
+++ src/main/java/com/example/SprintBoostTest02/SprintBoostTest02Application.java
@@ -0,0 +1,13 @@
+package com.example.SprintBoostTest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoostTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoostTest02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SprintBoostTest02/SprintBoostTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprintBoostTest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBoostTest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
